e39254367561d6bf5a36a3dadb01792ed5cc81d5
Nicholas Williams <nichwilliams@tesla.com>
2020-11-15 15:53:37 -0800

    error checking protocol

--- nurl/nurl.c
+++ nurl/nurl.c
@@ -5,12 +5,68 @@
 
 #include <errno.h>
 #include <netdb.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 
-int main() {
+void fail_invalid_url(char *url, char* reason) {
+  printf("Error: Invalid URL %s\n%s\n", url, reason);
+  exit(EXIT_FAILURE);
+}
+
+bool split_url(char *url) {
+  // What designates a valid url?
+  // [.a-zA-Z0-9].[a-zA-Z]/[a-zA-Z]
+
+  // Protocol Check
+  for (int i = 0; i < strlen(url); i++) {
+    char c = url[i];
+    // chars in C are ASCII so we're checking if c is within the bounds of all letters
+    bool is_letter = c >= 'A' && c <= 'z';
+    if (is_letter) {
+      continue;
+    } else if (c == ':') {
+      // http://
+      if (i + 2 > strlen(url) - 1) {
+        fail_invalid_url(url, "String ended before protocol definition completed");
+      } else if (url[i + 1] != '/' || url[i + 2] != '/') {
+        fail_invalid_url(url, "Protocol not formatted as '://'");
+      }
+      char protocol[i + 1];
+      memcpy(protocol, url, i);
+      protocol[i] = '\0';
+      if (strcmp(protocol, "http") != 0) {
+        char error[50 + i];
+        sprintf(error, "%s protocol not supported\nOnly supports http", protocol);
+        fail_invalid_url(url, error);
+      }
+      printf("http!\n");
+      break;
+    } else {
+        break;
+        printf("TODO\n");
+        fail_invalid_url(url, "todo");
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  // Pre-Steps
+  // 1 Validate # of args, only should be 2 (./nurl.out URL)
+  if (argc > 2) {
+    printf("Error: Too many arguments provided: %d\nUsage: nurl http://www.google.com\n", argc);
+    return EXIT_FAILURE;
+  } else if (argc < 2) {
+    printf("Error: Too few arguments provided: %d\nUsage: nurl http://www.google.com\n", argc);
+    return EXIT_FAILURE;
+  }
+
+  split_url(argv[1]);
+  return 1;
+
+
   // 1 Define server we want to talk to
   int sock;
   struct addrinfo *my_addr_info;
