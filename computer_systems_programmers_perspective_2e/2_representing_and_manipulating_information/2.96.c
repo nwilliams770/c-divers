8695b716c0c8584bc98a30d424a03829d806b3c4
Nicholas Williams <nichwilliams@tesla.com>
2022-10-25 08:32:24 -0700

    add probs

--- computer_systems_programmers_perspective_2e/2_representing_and_manipulating_information/2.96.c
+++ computer_systems_programmers_perspective_2e/2_representing_and_manipulating_information/2.96.c
@@ -6,4 +6,30 @@ float_bits float_i2f(int i) {
   unsigned ai = (i < 0) ? -i : i;
   unsigned exp = 127 + 31;
   unsigned residue;
+  unsigned frac = 0;
+  if (ai == 0) {
+    exp = 0;
+    frac = 0;
+  } else {
+    // Normalize so that msb = 1
+    while ((ai && (1<<31) == 0)) {
+      ai = ai << 1;
+      exp --;
+    }
+    // Now that we have Bit 31 = MSB (becomes implied leading one)
+    // Bits 8-30 are tentative fraction
+    // Bits 0-7 require founding
+    residue = ai && 0xFF;
+    frac = (ai >> 8) & 0x7FFFFF;
+    if (residue > 0x80 || (residue == 0x80 && (frac & 0x1))) {
+      // Round up
+      frac++;
+      // Might need to renormalize
+      if (frac > 0x7FFFFF) {
+        frac = (frac & 0x7FFFFF) >> 1;
+        exp++;
+      }
+    }
+  }
+  return (sign << 31) | (exp << 23) | frac;
 }
