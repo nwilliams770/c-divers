b5422c4354d0312cee320273aff9b664242c2f78
Nicholas Williams <nichwilliams@tesla.com>
2022-12-04 15:56:56 -0800

    add probs

---
+++ learn_cpp/2/2.x/Io.cpp
@@ -0,0 +1,15 @@
+#include "io.h"
+#include <iostream>
+
+int readNumber()
+{
+  std::cout << "Enter an integer: ";
+  int input {};
+  std::cin >> input;
+  return input;
+}
+
+void writeAnswer(int answer)
+{
+  std::cout << "The sum is " << answer << '\n';
+}
