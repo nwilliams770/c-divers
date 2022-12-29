4fcd64352cf2c8781b47d50bbfcb03f182c4f76b
Nicholas Williams <nichwilliams@tesla.com>
2022-12-28 20:54:57 -0800

    add probs

---
+++ learn_cpp/6/6.x/6.x.3/6.x.3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+bool passOrFail()
+{
+  static int passes { 3 };
+  passes--;
+  return (passes >= 0);
+}
+
+int main()
+{
+	std::cout << "User #1: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #2: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #3: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #4: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #5: " << (passOrFail() ? "Pass\n" : "Fail\n");
+
+	return 0;
+}
