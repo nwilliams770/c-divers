f3e4a5aeb05f0e5e021cb8b8d65ec0355025ee74
Nicholas Williams <nichwilliams@tesla.com>
2023-01-04 18:59:32 -0800

    add probs

---
+++ learn_cpp/7/7.x/7.x.3/7.x.3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+  return 0;
+}
+
+void run()
+{
+  // initialize twister and distribution
+
+  do
+  {
+
+  } while (playAgain());
+}
+
+void newGame()
+{
+
+}
+
+bool playAgain()
+{
+  return false;
+}
+
+void getInput()
+{
+
+}
