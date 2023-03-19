295b6da9e64d099cd1cf0af10420ab2b133a476c
Nicholas Williams <nichwilliams@tesla.com>
2023-03-19 12:24:17 -0700

    add probs

--- learn_cpp/17/17.x/17.x.3.cpp
+++ learn_cpp/17/17.x/17.x.3.cpp
@@ -1,5 +1,18 @@
+#include <array>
+#include <cstdlib> // for rand() and srand()
 #include <iostream>
 #include <string>
+
+
+// Generate a random number between min and max (inclusive)
+// Assumes std::srand() has already been called
+// Assumes max - min <= RAND_MAX
+int getRandomNumber(int min, int max)
+{
+  static constexpr double fraction{ 1.0 / (RAND_MAX + 1.0) }; // static used for efficiency, so we only calculate this value once
+  // evenly distribute the random number across our range
+  return min + static_cast<int>((max - min + 1) * (std::rand() * fraction));
+}
 
 class Creature
 {
@@ -51,12 +64,70 @@ public:
   {
   }
 
+  void levelUp()
+  {
+    ++m_level;
+    ++m_damage;
+  }
+
   int getLevel() const { return m_level; }
+  bool hasWon() const { return m_level >= 20; }
+};
+
+class Monster: public Creature
+{
+public:
+	enum class Type
+	{
+		dragon,
+		orc,
+		slime,
+		max_types
+	};
+
+private:
+  static const Creature& getDefaultCreature(Type type)
+  {
+    static const std::array<Creature, static_cast<std::size_t>(Type::max_types)> monsterData{
+      { { "dragon", 'D', 20, 4, 100 },
+        { "orc", 'o', 4, 2, 25 },
+        { "slime", 's', 1, 1, 10 } }
+    };
+
+    return monsterData.at(static_cast<std::size_t>(type));
+  }
+
+public:
+  Monster(Type type)
+    : Creature{ getDefaultCreature(type) }
+  {
+  }
+
+  static Monster getRandomMonster()
+  {
+    int num{ getRandomNumber(0, static_cast<int>(Type::max_types) - 1) };
+    return Monster{ static_cast<Type>(num) };
+  }
+};
+
+void fightMonster()
+{
+
+};
+
+void attackMonster()
+{
+
+};
+
+void attackPlayer()
+{
+
 };
 
 int main()
 {
-  std::cout << "Enter your name: "
+  std::cout << "Enter your name: ";
   std::string playerName;
   std::cin >> playerName;
 
@@ -64,9 +135,12 @@ int main()
 
   std::cout << "Welcome " << p.getName() << ".\n";
   std::cout << "You have " << p.getHealth() << " health and are carrying " <<
-    p.getGold() << " gold.\n"
+    p.getGold() << " gold.\n";
 
+  while (!p.isDead() && !p.hasWon())
+  {
 
+  }
 
   return 0;
 }
